4bf59d374d2c82c365cf3ad892d2e15d2c6379a3
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2022-09-12 14:13:30 +0000

    Add files via upload

--- Ren_Skidder_1.lua
+++ Ren_Skidder_1.lua
@@ -73,25 +73,19 @@ if BF then
 	repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
 	repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
 	repeat wait()
-		if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main").ChooseTeam.Visible == true then
-			if string.find(getgenv().SelectTeam, "Pirate") then
-				local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
-	
-				firesignal(ChooseTeam.Container["Pirates"].Frame.ViewportFrame.TextButton.Activated)
-				workspace.Camera.CameraType = Enum.CameraType.Custom
-				ChooseTeam.Visible = false
-			elseif string.find(getgenv().SelectTeam, "Marine")  then
-				local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
-	
-				firesignal(ChooseTeam.Container["Marines"].Frame.ViewportFrame.TextButton.Activated)
-				workspace.Camera.CameraType = Enum.CameraType.Custom
-				ChooseTeam.Visible = false
+		if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main"):FindFirstChild("ChooseTeam") then
+			if string.find(getgenv().Setting["Join Team"], "Pirate") then
+				for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.Activated)) do
+					v.Function()
+				end 
+			elseif string.find(getgenv().Setting["Join Team"], "Marine")  then
+				for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton.Activated)) do
+					v.Function()
+				end 
 			else
-				local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
-	
-				firesignal(ChooseTeam.Container["Pirates"].Frame.ViewportFrame.TextButton.Activated)
-				workspace.Camera.CameraType = Enum.CameraType.Custom
-				ChooseTeam.Visible = false
+				for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.Activated)) do
+					v.Function()
+				end 
 			end
 		end
 	until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
