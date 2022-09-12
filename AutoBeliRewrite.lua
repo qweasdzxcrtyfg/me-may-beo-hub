e4ab045b3bd47f2923add6d99edaa87fb7d963b1
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2022-09-12 13:31:24 +0000

    Update AutoBeliRewrite.lua

--- AutoBeliRewrite.lua
+++ AutoBeliRewrite.lua
@@ -14,17 +14,23 @@ print("Auto Beli Version: "..version)
 repeat wait()
     if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main").ChooseTeam.Visible == true then
         if string.find(getgenv().SelectTeam, "Pirate") then
-            for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.MouseButton1Click)) do
-                v.Function()
-            end
+            local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
+
+            firesignal(ChooseTeam.Container["Pirates"].Frame.ViewportFrame.TextButton.Activated)
+            workspace.Camera.CameraType = Enum.CameraType.Custom
+            ChooseTeam.Visible = false
         elseif string.find(getgenv().SelectTeam, "Marine")  then
-            for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton.MouseButton1Click)) do
-                v.Function()
-            end
+            local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
+
+            firesignal(ChooseTeam.Container["Marines"].Frame.ViewportFrame.TextButton.Activated)
+            workspace.Camera.CameraType = Enum.CameraType.Custom
+            ChooseTeam.Visible = false
         else
-            for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.MouseButton1Click)) do
-                v.Function()
-            end
+            local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
+
+            firesignal(ChooseTeam.Container["Pirates"].Frame.ViewportFrame.TextButton.Activated)
+            workspace.Camera.CameraType = Enum.CameraType.Custom
+            ChooseTeam.Visible = false
         end
     end
 until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
