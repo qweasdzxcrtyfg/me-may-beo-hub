b6f78a97d87c95fdae4f42ee26aa6add718344fc
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2022-10-09 08:04:37 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,13 @@
+return {
+    BanditQuest1 = { {
+        LevelReq = 0, 
+        Name = "Bandits", 
+        Task = {
+            Bandit = 5
+        }, 
+        Reward = {
+            Beli = 350, 
+            Exp = 300
+        }
+    } }, 
+}
