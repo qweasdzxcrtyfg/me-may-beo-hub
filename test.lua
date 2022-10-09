6529ee1a32f323f52459016bd2fa0b06475e99b7
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2022-10-09 08:12:33 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,1118 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
 return {
-    BanditQuest1 = { {
-        LevelReq = 0, 
-        Name = "Bandits", 
-        Task = {
-            Bandit = 5
-        }, 
-        Reward = {
-            Beli = 350, 
-            Exp = 300
-        }
-    } }, 
-}
+	BanditQuest1 = { {
+			LevelReq = 0, 
+			Name = "Bandits", 
+			Task = {
+				Bandit = 5
+			}, 
+			Reward = {
+				Beli = 350, 
+				Exp = 300
+			}
+		} }, 
+	MarineQuest = { {
+			LevelReq = 0, 
+			Name = "Trainees", 
+			Task = {
+				Trainee = 5
+			}, 
+			Reward = {
+				Beli = 350, 
+				Exp = 300
+			}
+		} }, 
+	JungleQuest = { {
+			LevelReq = 10, 
+			Name = "Monkeys", 
+			Task = {
+				Monkey = 6
+			}, 
+			Reward = {
+				Beli = 800, 
+				Exp = 2300
+			}
+		}, {
+			LevelReq = 15, 
+			Name = "Gorillas", 
+			Task = {
+				Gorilla = 8
+			}, 
+			Reward = {
+				Beli = 1200, 
+				Exp = 4500
+			}
+		}, {
+			LevelReq = 20, 
+			Name = "Gorilla King", 
+			Task = {
+				["The Gorilla King"] = 1
+			}, 
+			Reward = {
+				Beli = 2000, 
+				Exp = 9500
+			}
+		} }, 
+	BuggyQuest1 = { {
+			LevelReq = 30, 
+			Name = "Pirates", 
+			Task = {
+				Pirate = 8
+			}, 
+			Reward = {
+				Beli = 3000, 
+				Exp = 13000
+			}
+		}, {
+			LevelReq = 40, 
+			Name = "Brute", 
+			Task = {
+				Brute = 8
+			}, 
+			Reward = {
+				Beli = 3500, 
+				Exp = 22000
+			}
+		}, {
+			LevelReq = 55, 
+			Name = "Bobby", 
+			Task = {
+				Bobby = 1
+			}, 
+			Reward = {
+				Beli = 8000, 
+				Exp = 45000
+			}
+		} }, 
+	DesertQuest = { {
+			LevelReq = 60, 
+			Name = "Desert Bandit", 
+			Task = {
+				["Desert Bandit"] = 8
+			}, 
+			Reward = {
+				Beli = 4000, 
+				Exp = 45000
+			}
+		}, {
+			LevelReq = 75, 
+			Name = "Desert Officer", 
+			Task = {
+				["Desert Officer"] = 6
+			}, 
+			Reward = {
+				Beli = 4500, 
+				Exp = 65000
+			}
+		} }, 
+	SnowQuest = { {
+			LevelReq = 90, 
+			Name = "Snow Bandit", 
+			Task = {
+				["Snow Bandit"] = 7
+			}, 
+			Reward = {
+				Beli = 5000, 
+				Exp = 90000
+			}
+		}, {
+			LevelReq = 100, 
+			Name = "Snowman", 
+			Task = {
+				Snowman = 8
+			}, 
+			Reward = {
+				Beli = 5500, 
+				Exp = 150000
+			}
+		}, {
+			LevelReq = 105, 
+			Name = "Yeti", 
+			Task = {
+				Yeti = 1
+			}, 
+			Reward = {
+				Beli = 10000, 
+				Exp = 220000
+			}
+		} }, 
+	MarineQuest2 = { {
+			LevelReq = 120, 
+			Name = "Chief Petty Officer", 
+			Task = {
+				["Chief Petty Officer"] = 8
+			}, 
+			Reward = {
+				Beli = 6000, 
+				Exp = 225000
+			}
+		}, {
+			LevelReq = 130, 
+			Name = "Vice Admiral", 
+			Task = {
+				["Vice Admiral"] = 1
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 415000
+			}
+		} }, 
+	SkyQuest = { {
+			LevelReq = 150, 
+			Name = "Sky Bandit", 
+			Task = {
+				["Sky Bandit"] = 7
+			}, 
+			Reward = {
+				Beli = 7000, 
+				Exp = 315000
+			}
+		}, {
+			LevelReq = 175, 
+			Name = "Dark Master", 
+			Task = {
+				["Dark Master"] = 8
+			}, 
+			Reward = {
+				Beli = 7500, 
+				Exp = 450000
+			}
+		} }, 
+	PrisonerQuest = { {
+			LevelReq = 190, 
+			Name = "Prisoner", 
+			Task = {
+				Prisoner = 8
+			}, 
+			Reward = {
+				Beli = 7000, 
+				Exp = 550000
+			}
+		}, {
+			LevelReq = 210, 
+			Name = "Dangerous Prisoner", 
+			Task = {
+				["Dangerous Prisoner"] = 8
+			}, 
+			Reward = {
+				Beli = 7500, 
+				Exp = 780000
+			}
+		} }, 
+	ImpelQuest = { {
+			LevelReq = 220, 
+			Name = "Warden", 
+			Task = {
+				Warden = 1
+			}, 
+			Reward = {
+				Beli = 6000, 
+				Exp = 850000
+			}
+		}, {
+			LevelReq = 230, 
+			Name = "Chief Warden", 
+			Task = {
+				["Chief Warden"] = 1
+			}, 
+			Reward = {
+				Beli = 10000, 
+				Exp = 1000000
+			}
+		}, {
+			LevelReq = 240, 
+			Name = "Swan", 
+			Task = {
+				Swan = 1
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 1600000
+			}
+		} }, 
+	ColosseumQuest = { {
+			LevelReq = 250, 
+			Name = "Toga Warrior", 
+			Task = {
+				["Toga Warrior"] = 7
+			}, 
+			Reward = {
+				Beli = 7000, 
+				Exp = 1100000
+			}
+		}, {
+			LevelReq = 275, 
+			Name = "Gladiator", 
+			Task = {
+				Gladiator = 8
+			}, 
+			Reward = {
+				Beli = 7500, 
+				Exp = 1300000
+			}
+		} }, 
+	MagmaQuest = { {
+			LevelReq = 300, 
+			Name = "Mil. Soldier", 
+			Task = {
+				["Military Soldier"] = 7
+			}, 
+			Reward = {
+				Beli = 8250, 
+				Exp = 1700000
+			}
+		}, {
+			LevelReq = 325, 
+			Name = "Mil. Spy", 
+			Task = {
+				["Military Spy"] = 8
+			}, 
+			Reward = {
+				Beli = 8500, 
+				Exp = 2000000
+			}
+		}, {
+			LevelReq = 350, 
+			Name = "Magma Admiral", 
+			Task = {
+				["Magma Admiral"] = 1
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 3000000
+			}
+		} }, 
+	FishmanQuest = { {
+			LevelReq = 375, 
+			Name = "Fishman Warrior", 
+			Task = {
+				["Fishman Warrior"] = 8
+			}, 
+			Reward = {
+				Beli = 8750, 
+				Exp = 3050000
+			}
+		}, {
+			LevelReq = 400, 
+			Name = "Fishman Commando", 
+			Task = {
+				["Fishman Commando"] = 7
+			}, 
+			Reward = {
+				Beli = 9000, 
+				Exp = 3350000
+			}
+		}, {
+			LevelReq = 425, 
+			Name = "Fishman Lord", 
+			Task = {
+				["Fishman Lord"] = 1
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 4250000
+			}
+		} }, 
+	SkyExp1Quest = { {
+			LevelReq = 450, 
+			Name = "God's Guard", 
+			Task = {
+				["God's Guard"] = 7
+			}, 
+			Reward = {
+				Beli = 8750, 
+				Exp = 4250000
+			}
+		}, {
+			LevelReq = 475, 
+			Name = "Shanda", 
+			Task = {
+				Shanda = 9
+			}, 
+			Reward = {
+				Beli = 9000, 
+				Exp = 5000000
+			}
+		}, {
+			LevelReq = 500, 
+			Name = "Wysper", 
+			Task = {
+				Wysper = 1
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 5700000
+			}
+		} }, 
+	SkyExp2Quest = { {
+			LevelReq = 525, 
+			Name = "Royal Squad", 
+			Task = {
+				["Royal Squad"] = 8
+			}, 
+			Reward = {
+				Beli = 9500, 
+				Exp = 5800000
+			}
+		}, {
+			LevelReq = 550, 
+			Name = "Royal Soldier", 
+			Task = {
+				["Royal Soldier"] = 8
+			}, 
+			Reward = {
+				Beli = 9750, 
+				Exp = 6300000
+			}
+		}, {
+			LevelReq = 575, 
+			Name = "Thunder God", 
+			Task = {
+				["Thunder God"] = 1
+			}, 
+			Reward = {
+				Beli = 20000, 
+				Exp = 8000000
+			}
+		} }, 
+	FountainQuest = { {
+			LevelReq = 625, 
+			Name = "Galley Pirate", 
+			Task = {
+				["Galley Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 10000, 
+				Exp = 7500000
+			}
+		}, {
+			LevelReq = 650, 
+			Name = "Galley Captain", 
+			Task = {
+				["Galley Captain"] = 9
+			}, 
+			Reward = {
+				Beli = 10000, 
+				Exp = 8500000
+			}
+		}, {
+			LevelReq = 675, 
+			Name = "Cyborg", 
+			Task = {
+				Cyborg = 1
+			}, 
+			Reward = {
+				Beli = 20000, 
+				Exp = 10000000
+			}
+		} }, 
+	Area1Quest = { {
+			LevelReq = 700, 
+			Name = "Raider", 
+			Task = {
+				Raider = 8
+			}, 
+			Reward = {
+				Beli = 10250, 
+				Exp = 8750000
+			}
+		}, {
+			LevelReq = 725, 
+			Name = "Mercenary", 
+			Task = {
+				Mercenary = 8
+			}, 
+			Reward = {
+				Beli = 10500, 
+				Exp = 9750000
+			}
+		}, {
+			LevelReq = 750, 
+			Name = "Diamond", 
+			Task = {
+				Diamond = 1
+			}, 
+			Reward = {
+				Beli = 25000, 
+				Exp = 12500000
+			}
+		} }, 
+	Area2Quest = { {
+			LevelReq = 775, 
+			Name = "Swan Pirate", 
+			Task = {
+				["Swan Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 10750, 
+				Exp = 11500000
+			}
+		}, {
+			LevelReq = 800, 
+			Name = "Factory Staff", 
+			Task = {
+				["Factory Staff"] = 8
+			}, 
+			Reward = {
+				Beli = 11000, 
+				Exp = 13000000
+			}
+		}, {
+			LevelReq = 850, 
+			Name = "Jeremy", 
+			Task = {
+				Jeremy = 1
+			}, 
+			Reward = {
+				Beli = 25000, 
+				Exp = 16000000
+			}
+		} }, 
+	MarineQuest3 = { {
+			LevelReq = 875, 
+			Name = "Marine Lieutenant", 
+			Task = {
+				["Marine Lieutenant"] = 8
+			}, 
+			Reward = {
+				Beli = 11250, 
+				Exp = 15000000
+			}
+		}, {
+			LevelReq = 900, 
+			Name = "Marine Captain", 
+			Task = {
+				["Marine Captain"] = 9
+			}, 
+			Reward = {
+				Beli = 11500, 
+				Exp = 16500000
+			}
+		}, {
+			LevelReq = 925, 
+			Name = "Fajita", 
+			Task = {
+				Fajita = 1
+			}, 
+			Reward = {
+				Beli = 25000, 
+				Exp = 19000000
+			}
+		} }, 
+	ZombieQuest = { {
+			LevelReq = 950, 
+			Name = "Zombie", 
+			Task = {
+				Zombie = 8
+			}, 
+			Reward = {
+				Beli = 11750, 
+				Exp = 19000000
+			}
+		}, {
+			LevelReq = 975, 
+			Name = "Vampire", 
+			Task = {
+				Vampire = 8
+			}, 
+			Reward = {
+				Beli = 12000, 
+				Exp = 20500000
+			}
+		} }, 
+	SnowMountainQuest = { {
+			LevelReq = 1000, 
+			Name = "Snow Trooper", 
+			Task = {
+				["Snow Trooper"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 22500000
+			}
+		}, {
+			LevelReq = 1050, 
+			Name = "Winter Warrior", 
+			Task = {
+				["Winter Warrior"] = 9
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 24000000
+			}
+		} }, 
+	IceSideQuest = { {
+			LevelReq = 1100, 
+			Name = "Lab Subordinate", 
+			Task = {
+				["Lab Subordinate"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 25500000
+			}
+		}, {
+			LevelReq = 1125, 
+			Name = "Horned Warrior", 
+			Task = {
+				["Horned Warrior"] = 9
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 27000000
+			}
+		}, {
+			LevelReq = 1150, 
+			Name = "Smoke Admiral", 
+			Task = {
+				["Smoke Admiral"] = 1
+			}, 
+			Reward = {
+				Beli = 20000, 
+				Exp = 32500000
+			}
+		} }, 
+	FireSideQuest = { {
+			LevelReq = 1175, 
+			Name = "Magma Ninja", 
+			Task = {
+				["Magma Ninja"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 29000000
+			}
+		}, {
+			LevelReq = 1200, 
+			Name = "Lava Pirate", 
+			Task = {
+				["Lava Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 31000000
+			}
+		} }, 
+	ShipQuest1 = { {
+			LevelReq = 1250, 
+			Name = "Ship Deckhand", 
+			Task = {
+				["Ship Deckhand"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 33000000
+			}
+		}, {
+			LevelReq = 1275, 
+			Name = "Ship Engineer", 
+			Task = {
+				["Ship Engineer"] = 8
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 35500000
+			}
+		} }, 
+	ShipQuest2 = { {
+			LevelReq = 1300, 
+			Name = "Ship Steward", 
+			Task = {
+				["Ship Steward"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 37500000
+			}
+		}, {
+			LevelReq = 1325, 
+			Name = "Ship Officer", 
+			Task = {
+				["Ship Officer"] = 8
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 39500000
+			}
+		} }, 
+	FrostQuest = { {
+			LevelReq = 1350, 
+			Name = "Arctic Warrior", 
+			Task = {
+				["Arctic Warrior"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 41000000
+			}
+		}, {
+			LevelReq = 1375, 
+			Name = "Snow Lurker", 
+			Task = {
+				["Snow Lurker"] = 8
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 43000000
+			}
+		}, {
+			LevelReq = 1400, 
+			Name = "Ice Admiral", 
+			Task = {
+				["Awakened Ice Admiral"] = 1
+			}, 
+			Reward = {
+				Beli = 20000, 
+				Exp = 45000000
+			}
+		} }, 
+	ForgottenQuest = { {
+			LevelReq = 1425, 
+			Name = "Sea Soldier", 
+			Task = {
+				["Sea Soldier"] = 8
+			}, 
+			Reward = {
+				Beli = 12250, 
+				Exp = 47000000
+			}
+		}, {
+			LevelReq = 1450, 
+			Name = "Water Fighter", 
+			Task = {
+				["Water Fighter"] = 8
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 49000000
+			}
+		}, {
+			LevelReq = 1475, 
+			Name = "Tide Keeper", 
+			Task = {
+				["Tide Keeper"] = 1
+			}, 
+			Reward = {
+				Beli = 12500, 
+				Exp = 51000000
+			}
+		} }, 
+	PiratePortQuest = { {
+			LevelReq = 1500, 
+			Name = "Pirate Millionaire", 
+			Task = {
+				["Pirate Millionaire"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 53000000
+			}
+		}, {
+			LevelReq = 1525, 
+			Name = "Pistol Billionaire", 
+			Task = {
+				["Pistol Billionaire"] = 8
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 55500000
+			}
+		}, {
+			LevelReq = 1550, 
+			Name = "Stone", 
+			Task = {
+				Stone = 1
+			}, 
+			Reward = {
+				Beli = 25000, 
+				Exp = 60000000
+			}
+		} }, 
+	AmazonQuest = { {
+			LevelReq = 1575, 
+			Name = "Dragon Crew Warrior", 
+			Task = {
+				["Dragon Crew Warrior"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 58000000
+			}
+		}, {
+			LevelReq = 1600, 
+			Name = "Dragon Crew Archer", 
+			Task = {
+				["Dragon Crew Archer"] = 8
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 60500000
+			}
+		} }, 
+	AmazonQuest2 = { {
+			LevelReq = 1625, 
+			Name = "Female Islander", 
+			Task = {
+				["Female Islander"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 62500000
+			}
+		}, {
+			LevelReq = 1650, 
+			Name = "Giant Islander", 
+			Task = {
+				["Giant Islander"] = 8
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 64500000
+			}
+		}, {
+			LevelReq = 1675, 
+			Name = "Island Empress", 
+			Task = {
+				["Island Empress"] = 1
+			}, 
+			Reward = {
+				Beli = 30000, 
+				Exp = 70000000
+			}
+		} }, 
+	MarineTreeIsland = { {
+			LevelReq = 1700, 
+			Name = "Marine Commodore", 
+			Task = {
+				["Marine Commodore"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 68000000
+			}
+		}, {
+			LevelReq = 1725, 
+			Name = "Marine Rear Admiral", 
+			Task = {
+				["Marine Rear Admiral"] = 8
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 70500000
+			}
+		}, {
+			LevelReq = 1750, 
+			Name = "Kilo Admiral", 
+			Task = {
+				["Kilo Admiral"] = 1
+			}, 
+			Reward = {
+				Beli = 35000, 
+				Exp = 78000000
+			}
+		} }, 
+	DeepForestIsland3 = { {
+			LevelReq = 1775, 
+			Name = "Fishman Raider", 
+			Task = {
+				["Fishman Raider"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 73000000
+			}
+		}, {
+			LevelReq = 1800, 
+			Name = "Fishman Captain", 
+			Task = {
+				["Fishman Captain"] = 8
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 75500000
+			}
+		} }, 
+	DeepForestIsland = { {
+			LevelReq = 1825, 
+			Name = "Forest Pirate", 
+			Task = {
+				["Forest Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 78000000
+			}
+		}, {
+			LevelReq = 1850, 
+			Name = "Mythological Pirate", 
+			Task = {
+				["Mythological Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 81000000
+			}
+		}, {
+			LevelReq = 1875, 
+			Name = "Captain Elephant", 
+			Task = {
+				["Captain Elephant"] = 1
+			}, 
+			Reward = {
+				Beli = 40000, 
+				Exp = 90000000
+			}
+		} }, 
+	DeepForestIsland2 = { {
+			LevelReq = 1900, 
+			Name = "Jungle Pirate", 
+			Task = {
+				["Jungle Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 85000000
+			}
+		}, {
+			LevelReq = 1925, 
+			Name = "Musketeer Pirate", 
+			Task = {
+				["Musketeer Pirate"] = 8
+			}, 
+			Reward = {
+				Beli = 15000, 
+				Exp = 87500000
+			}
+		}, {
+			LevelReq = 1950, 
+			Name = "Beautiful Pirate", 
+			Task = {
+				["Beautiful Pirate"] = 1
+			}, 
+			Reward = {
+				Beli = 50000, 
+				Exp = 100000000
+			}
+		} }, 
+	HauntedQuest1 = { {
+			LevelReq = 1975, 
+			Name = "Reborn Skeleton", 
+			Task = {
+				["Reborn Skeleton"] = 8
+			}, 
+			Reward = {
+				Beli = 13000, 
+				Exp = 91000000
+			}
+		}, {
+			LevelReq = 2000, 
+			Name = "Living Zombie", 
+			Task = {
+				["Living Zombie"] = 8
+			}, 
+			Reward = {
+				Beli = 13250, 
+				Exp = 93500000
+			}
+		} }, 
+	HauntedQuest2 = { {
+			LevelReq = 2025, 
+			Name = "Demonic Soul", 
+			Task = {
+				["Demonic Soul"] = 8
+			}, 
+			Reward = {
+				Beli = 13500, 
+				Exp = 96000000
+			}
+		}, {
+			LevelReq = 2050, 
+			Name = "Posessed Mummy", 
+			Task = {
+				["Posessed Mummy"] = 8
+			}, 
+			Reward = {
+				Beli = 13750, 
+				Exp = 98500000
+			}
+		} }, 
+	NutsIslandQuest = { {
+			LevelReq = 2075, 
+			Name = "Peanut Scout", 
+			Task = {
+				["Peanut Scout"] = 8
+			}, 
+			Reward = {
+				Beli = 14000, 
+				Exp = 100000000
+			}
+		}, {
+			LevelReq = 2100, 
+			Name = "Peanut President", 
+			Task = {
+				["Peanut President"] = 8
+			}, 
+			Reward = {
+				Beli = 14100, 
+				Exp = 102500000
+			}
+		} }, 
+	IceCreamIslandQuest = { {
+			LevelReq = 2125, 
+			Name = "Ice Cream Chef", 
+			Task = {
+				["Ice Cream Chef"] = 8
+			}, 
+			Reward = {
+				Beli = 14200, 
+				Exp = 105000000
+			}
+		}, {
+			LevelReq = 2150, 
+			Name = "Ice Cream Commander", 
+			Task = {
+				["Ice Cream Commander"] = 8
+			}, 
+			Reward = {
+				Beli = 14300, 
+				Exp = 107500000
+			}
+		}, {
+			LevelReq = 2175, 
+			Name = "Cake Queen", 
+			Task = {
+				["Cake Queen"] = 1
+			}, 
+			Reward = {
+				Beli = 30000, 
+				Exp = 112500000
+			}
+		} }, 
+	CakeQuest1 = { {
+			LevelReq = 2200, 
+			Name = "Cookie Crafter", 
+			Task = {
+				["Cookie Crafter"] = 8
+			}, 
+			Reward = {
+				Beli = 14200, 
+				Exp = 110000000
+			}
+		}, {
+			LevelReq = 2225, 
+			Name = "Cake Guard", 
+			Task = {
+				["Cake Guard"] = 8
+			}, 
+			Reward = {
+				Beli = 14300, 
+				Exp = 112500000
+			}
+		} }, 
+	CakeQuest2 = { {
+			LevelReq = 2250, 
+			Name = "Baking Staff", 
+			Task = {
+				["Baking Staff"] = 8
+			}, 
+			Reward = {
+				Beli = 14400, 
+				Exp = 115000000
+			}
+		}, {
+			LevelReq = 2275, 
+			Name = "Head Baker", 
+			Task = {
+				["Head Baker"] = 8
+			}, 
+			Reward = {
+				Beli = 14500, 
+				Exp = 117500000
+			}
+		} }, 
+	ChocQuest1 = { {
+			LevelReq = 2300, 
+			Name = "Cocoa Warrior", 
+			Task = {
+				["Cocoa Warrior"] = 8
+			}, 
+			Reward = {
+				Beli = 14600, 
+				Exp = 120000000
+			}
+		}, {
+			LevelReq = 2325, 
+			Name = "Chocolate Bar Battler", 
+			Task = {
+				["Chocolate Bar Battler"] = 8
+			}, 
+			Reward = {
+				Beli = 14700, 
+				Exp = 122500000
+			}
+		} }, 
+	ChocQuest2 = { {
+			LevelReq = 2350, 
+			Name = "Sweet Thief", 
+			Task = {
+				["Sweet Thief"] = 8
+			}, 
+			Reward = {
+				Beli = 14800, 
+				Exp = 125000000
+			}
+		}, {
+			LevelReq = 2375, 
+			Name = "Candy Rebel", 
+			Task = {
+				["Candy Rebel"] = 8
+			}, 
+			Reward = {
+				Beli = 14900, 
+				Exp = 127500000
+			}
+		} }, 
+	BartiloQuest = { {
+			LevelReq = 850, 
+			Name = "Swan's Raid", 
+			Task = {
+				["Swan Pirate"] = 50
+			}, 
+			Reward = {
+				Beli = 50000, 
+				Exp = 35000000
+			}, 
+			MeetsRequirements = function(p1)
+				return not p1.getSession().Data.BartiloQuest.KilledBandits;
+			end, 
+			OnComplete = function(p2)
+				local l__Data__1 = p2.getSession().Data;
+				if not l__Data__1.BartiloQuest.KilledBandits then
+					l__Data__1.BartiloQuest.KilledBandits = true;
+					p2.notify("Well done! Talk to Bartilo again.");
+				end;
+			end
+		} }, 
+	CitizenQuest = { {
+			LevelReq = 1800, 
+			Name = "Town Raid", 
+			Task = {
+				["Forest Pirate"] = 50
+			}, 
+			Reward = {
+				Beli = 100000, 
+				Exp = 250000000
+			}, 
+			MeetsRequirements = function(p3)
+				return not p3.getSession().Data.CitizenQuest.KilledBandits;
+			end, 
+			OnComplete = function(p4)
+				local l__Data__2 = p4.getSession().Data;
+				if not l__Data__2.CitizenQuest.KilledBandits then
+					l__Data__2.CitizenQuest.KilledBandits = true;
+					p4.notify("Well done! Talk to Citizen again.");
+				end;
+			end
+		} }
+};
