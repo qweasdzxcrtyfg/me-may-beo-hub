eab2dbb62a000841dda5d508bbbfb132c7d85aed
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2022-11-11 05:05:02 +0700

    Add files via upload

--- RAMCHECK2.lua
+++ RAMCHECK2.lua
@@ -1,6 +1,4 @@
 getgenv().ChooseTeam = "Pirate"
-getgenv().DiscordId = "884662992921313352"--string not number sir
-getgenv().WebhookUrl = "https://discord.com/api/webhooks/1030340611162779668/nQpDzHm8Y2SKhmifcfxieDXzAmjFg__qgbNB0FQRXSQmkbqPnmnv14mRRIHcxZeAIxNY"
 getgenv().FruitInventoryRarity = 3 -->= 3
 getgenv().InventoryRarity = 4 -->= 4
 repeat wait() until game:IsLoaded()
@@ -28,168 +26,96 @@ repeat wait()
     end
 until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
 wait(3)
-function CheckDevilFruit()
-    local mmb1 = "Devil Fruit: Nothing"
-    local toreturn2 = ""
-    if game:GetService("Players").LocalPlayer.Data.DevilFruit.Value == "" then
-        mmb1 = "Devil Fruit: Nothing"
-    else
-        function checkmasterydf()
-            mas = 0
-            for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if v:IsA("Tool") and v.ToolTip == "Blox Fruit" then
-                    mas = v.Level.Value
-                end
-            end
-            for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
-                if v:IsA("Tool") and v.ToolTip == "Blox Fruit" then
-                    mas = v.Level.Value
-                end
-            end
-            return mas;
-        end
-        mmb1 = "Fruit: "..tostring(game:GetService("Players").LocalPlayer.Data.DevilFruit.Value.." | Mastery: "..checkmasterydf().." | ")
-        df = game:GetService("Players").LocalPlayer.Data.DevilFruit.Value
-        Raids = require(game:GetService("ReplicatedStorage").Raids).raids
-        AdvancedRaids = require(game:GetService("ReplicatedStorage").Raids).advancedRaids
-        Raids2 = {}
-        for i,v in pairs(Raids) do
-            table.insert(Raids2,v)
-        end
-        for i,v in pairs(AdvancedRaids) do
-            table.insert(Raids2,v)
-        end
-        function khongngu22(name)
-            string22 = name:split("-")
-            return string22[2]
-        end
-        if table.find(Raids2,khongngu22(game:GetService("Players").LocalPlayer.Data.DevilFruit.Value)) then
-            Awaked = {}
-            for i,v in pairs(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getAwakenedAbilities")) do
-                if not Awaked[i] then Awaked[i] = {["Awakened"] = v["Awakened"]} end
-            end
-            toreturn2 = "Awaked Skills: "
-            New2 = {}
-            for i,v in next, Awaked do
-                if v["Awakened"] == true then
-                    New2[i] = i..", "
-                end
-            end
-            for i,v in next, New2 do
-                toreturn2 = toreturn2..v
-            end
+local RAMAccount = loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RAMAccount.lua')()
+
+local MyAccount = RAMAccount.new(game:GetService'Players'.LocalPlayer.Name)
+
+if MyAccount then
+    function CheckDevilFruit()
+        local mmb1 = "Devil Fruit: Nothing"
+        local toreturn2 = ""
+        if game:GetService("Players").LocalPlayer.Data.DevilFruit.Value == "" then
+            mmb1 = "Devil Fruit: Nothing"
         else
-            mmb1 = mmb1.."Cant Awake."
-        end
-    end
-    return mmb1..""..toreturn2;
-end 
-function CheckItem(itemcc)
-    cac = false
-    for k,v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
-        if v.Name == itemcc then 
-            return true;
-        end
-    end
-    return cac;
-end
-function GetListWeapon(Rarity)
-    khongngu33 = ""
-    for k,v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
-        if v["Rarity"] >= Rarity and v["Type"] ~= "Material" and v["Type"] ~= "Blox Fruit" then
-            khongngu33 = khongngu33..v["Name"]..", "
-        end
-    end
-    return khongngu33;
-end
-function MeleeCount()
-    meleecoss = 0
-    meleetable = {"DragonTalon","Superhuman","Godhuman","ElectricClaw","SharkmanKarate", "DeathStep",}
-    for i,v in pairs(meleetable) do
-        if game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buy"..v,true) == 1 then
-            meleecoss = meleecoss + 1
-        end
-    end
-    return meleecoss;
-end
-function getInventoryFruits(Rarity)
-    khongngu44 = ""
-    for k,v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
-        if v["Rarity"] >= Rarity and v["Type"] == "Blox Fruit" then
-            khongngu44 = khongngu44..v["Name"]..", "
-        end
-    end
-    return khongngu44;
-end
-function isfullskill()
-    ff = false
-    Raids = require(game:GetService("ReplicatedStorage").Raids).raids
-    AdvancedRaids = require(game:GetService("ReplicatedStorage").Raids).advancedRaids
-    Raids2 = {}
-    for i,v in pairs(Raids) do
-        table.insert(Raids2,v)
-    end
-    for i,v in pairs(AdvancedRaids) do
-        table.insert(Raids2,v)
-    end
-    function khongngu22(name)
-        string22 = name:split("-")
-        return string22[2]
-    end
-    if table.find(Raids2,khongngu22(game:GetService("Players").LocalPlayer.Data.DevilFruit.Value)) then
-        Awaked = {}
-        c = 0
-        d = 0
-        for i,v in pairs(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getAwakenedAbilities")) do
-            if not Awaked[i] then Awaked[i] = {["Awakened"] = v["Awakened"]} end
-            c = c + 1
-        end
-        for k,v in pairs(Awaked) do
-            if v["Awakened"] == true then
-                d = d+1
+            function checkmasterydf()
+                mas = 0
+                for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                    if v:IsA("Tool") and v.ToolTip == "Blox Fruit" then
+                        mas = v.Level.Value
+                    end
+                end
+                for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                    if v:IsA("Tool") and v.ToolTip == "Blox Fruit" then
+                        mas = v.Level.Value
+                    end
+                end
+                return mas;
+            end
+            mmb1 = "Fruit: "..tostring(game:GetService("Players").LocalPlayer.Data.DevilFruit.Value.." | Mastery: "..checkmasterydf().." | ")
+            df = game:GetService("Players").LocalPlayer.Data.DevilFruit.Value
+            Raids = require(game:GetService("ReplicatedStorage").Raids).raids
+            AdvancedRaids = require(game:GetService("ReplicatedStorage").Raids).advancedRaids
+            Raids2 = {}
+            for i,v in pairs(Raids) do
+                table.insert(Raids2,v)
+            end
+            for i,v in pairs(AdvancedRaids) do
+                table.insert(Raids2,v)
+            end
+            function khongngu22(name)
+                string22 = name:split("-")
+                return string22[2]
+            end
+            if table.find(Raids2,khongngu22(game:GetService("Players").LocalPlayer.Data.DevilFruit.Value)) then
+                Awaked = {}
+                for i,v in pairs(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getAwakenedAbilities")) do
+                    if not Awaked[i] then Awaked[i] = {["Awakened"] = v["Awakened"]} end
+                end
+                toreturn2 = "Awaked Skills: "
+                New2 = {}
+                for i,v in next, Awaked do
+                    if v["Awakened"] == true then
+                        New2[i] = i..", "
+                    end
+                end
+                for i,v in next, New2 do
+                    toreturn2 = toreturn2..v
+                end
+            else
+                mmb1 = mmb1.."Cant Awake."
             end
         end
-        if d >= c then 
-            ff = true
-        else
-            ff = false
+        return mmb1..""..toreturn2;
+    end 
+    print("Loaded Account.\nAccount: "..game:GetService'Players'.LocalPlayer.Name)
+    function GetListWeapon(Rarity)
+        khongngu33 = ""
+        for k,v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
+            if v["Rarity"] >= Rarity and v["Type"] ~= "Material" and v["Type"] ~= "Blox Fruit" then
+                khongngu33 = khongngu33..v["Name"]..", "
+            end
         end
+        return khongngu33;
     end
-    return ff;
-end
-repeat 
-    wait()
-until game:IsLoaded() and tonumber(MeleeCount()) >= 6 and CheckItem("Cursed Dual Katana") and isfullskill()
-print(tonumber(MeleeCount()))
-print(tostring(CheckItem("Cursed Dual Katana")))
-print(tostring(isfullskill()))
-local webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/memaybeohub/me-may-beo-hub/main/Check%20Account%20Status.lua"))()
-webhook:SetUrl(getgenv().WebhookUrl)
-webhook:SetCustomMessage("<@"..getgenv().DiscordId.."> ")
-local send22k = "taokhongngumayngu.json"
-issent = {}
-notsent = {}
-issentread = {}
-if isfile(send22k) then
-    issentread = game:GetService("HttpService"):JSONDecode(readfile(send22k))
-    issentread2 = {}
-    for i,v in pairs(issentread) do
-        table.insert(issentread2,i)
-    end
-    if table.find(issentread2,game.Players.LocalPlayer.Name) then
-        if issentread[game.Players.LocalPlayer.Name] == true then
-            repeat wait() until issentread[game.Players.LocalPlayer.Name] == false or issentread[game.Players.LocalPlayer.Name] == nil
-            webhook:Send()
-        else
-            webhook:Send()
-            issentread[game.Players.LocalPlayer.Name] = true
-            writefile(send22k, game:GetService("HttpService"):JSONEncode(issentread))
+    function MeleeCount()
+        meleecoss = 0
+        meleetable = {"DragonTalon","Superhuman","Godhuman","ElectricClaw","SharkmanKarate", "DeathStep",}
+        for i,v in pairs(meleetable) do
+            if game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buy"..v,true) == 1 then
+                meleecoss = meleecoss + 1
+            end
         end
-    else
-        issentread[game.Players.LocalPlayer.Name] = false
-        writefile(send22k, game:GetService("HttpService"):JSONEncode(issentread))
+        return meleecoss;
     end
-else
-    writefile(send22k,issent)
+    function getInventoryFruits(Rarity)
+        khongngu44 = ""
+        for k,v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
+            if v["Rarity"] >= Rarity and v["Type"] == "Blox Fruit" then
+                khongngu44 = khongngu44..v["Name"]..", "
+            end
+        end
+        return khongngu44;
+    end
+    MyAccount:SetAlias("Level: "..game.Players.LocalPlayer.Data.Level.Value.." | "..MeleeCount())
+    MyAccount:SetDescription("Inventory: "..GetListWeapon(getgenv().InventoryRarity).."\nInventory Fruit: "..getInventoryFruits(getgenv().FruitInventoryRarity).."\n"..CheckDevilFruit())
 end
---("Level: "..game.Players.LocalPlayer.Data.Level.Value.." | "..MeleeCount().."Inventory: "..GetListWeapon(getgenv().InventoryRarity).."\nInventory Fruit: "..getInventoryFruits(getgenv().FruitInventoryRarity).."\n"..CheckDevilFruit())
